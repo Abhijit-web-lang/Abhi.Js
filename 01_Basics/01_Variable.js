33423256debfb4de3c93369ca93734f4b2a7f116
Abhijit-web-lang <abhijitprasad2003@gmail.com>
2025-12-30 06:46:32 +0000

    Variables

---
+++ 01_Basics/01_Variable.js
@@ -0,0 +1,13 @@
+const accountId = 2588456
+let accountEmail = "abhijitprasad2003@gmail.com "
+var accountPassword = "87945625"
+accountCity = "Patna"
+// accountState;
+
+//const accountId = 5456255 //This is not allowed
+ accountEmail = "prasadabhijit09@gmail.com"
+ accountPassword = "123456789"
+ accountCity = "Jehanabad"
+
+ console.table([accountEmail, accountId, accountPassword, accountCity, ])
+
