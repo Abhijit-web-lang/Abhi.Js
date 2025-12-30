aefb225690bf4de1dca9f287c88cc7f4131adb80
Abhijit-web-lang <abhijitprasad2003@gmail.com>
2025-12-30 06:58:57 +0000

    DataTypes

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+"Use strict";  //  Treat all js code as new version
+
+//alert (3+3)   //We are using node.js not browser
+
+console.log(3
+    +
+    3);  // code readability should high
+
+    let name = "Abhijit"
+    let age = 23
+    let isLoggedIn = false
+
+
+    // number => 2 to powwer 53
+    // bigInt
+    // string => "  "
+    // boolean => true/false
+    // null => standalone value
+    // undefined => 
+    // symbol => unique 
+
+
+    // object
+
+    
+    console.log(typeof null);
+    console.log(typeof undefined);
+    
+
