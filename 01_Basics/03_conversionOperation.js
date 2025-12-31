99387f5d7cc5eb64e648ed794b5f0e96d7a3b1ed
Abhijit-web-lang <abhijitprasad2003@gmail.com>
2025-12-31 04:14:41 +0000

    ConversionOfDataTypes

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,50 @@
+// let score = "55"
+// console.log(typeof score);
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+ 
+
+// let id = "null"
+// console.log(typeof id);
+// let valueInNumber = Number(id)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber)
+
+// let id = "undefined"
+// console.log(typeof id);
+// let valueInNumber = Number(id)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber)
+
+let id = "true"
+console.log(typeof id);
+let valueInNumber = Number(id)
+console.log(typeof valueInNumber);
+console.log(valueInNumber)
+
+
+
+
+
+
+// "55" => 55
+// "55abc" => NaN
+// true => 1; false => 0
+
+
+let isLoggedIn = "Shyam"
+
+let booleanisLoggedIn = Boolean(isLoggedIn)
+console.log(booleanisLoggedIn);
+
+// 1 => true
+// 0 => false
+// " " => false
+//"shyam" => false
+
+
+let someNumber = 55
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
