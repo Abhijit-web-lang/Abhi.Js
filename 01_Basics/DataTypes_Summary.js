0a9132751bc7a021c9b5c36481966adb623574bd
Abhijit-web-lang <abhijitprasad2003@gmail.com>
2025-12-31 05:52:36 +0000

    DataTypeSummary

---
+++ 01_Basics/DataTypes_Summary.js
@@ -0,0 +1,44 @@
+// # PRIMITIVE
+
+// 7 Types : String, Number, Boolean, Null, Undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.2
+const isLoggedIn = false
+const outSideTemperature = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+console.log(id === anotherId);
+
+const bigNumber = 5554564684446n
+
+
+
+
+
+
+
+
+// Reference Type (Non Primitive)
+
+
+// Arrays, Objects, Functions
+
+const heroes = ["Shaktiman", "NaagRaj", "Doga"]
+
+// let myObj{
+//     name : Shyam,
+//     age : 23,
+// }
+
+
+ const myFunction = function(){
+    console.log("Hello World");
+ }
+
+
+ console.log(typeof bigNumber);
+ console.log(typeof outSideTemperature);
+ console.log(typeof heroes);
