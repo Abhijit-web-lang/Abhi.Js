23b6a3b4a37d7552b8ee531bb44c5dc705c31e3e
Abhijit-web-lang <abhijitprasad2003@gmail.com>
2026-01-02 03:30:19 +0000

    String

---
+++ 01_Basics/05_String.js
@@ -0,0 +1,15 @@
+const name = "Abhijit"
+const repoCount = " 5"
+
+//console.log(name + repoCount + " Value");
+
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}.`);
+
+
+let gameName = new String ('AbhijitShy')
+
+console.log(gameName[0] )
+console.log(gameName.__proto__);
+
+
